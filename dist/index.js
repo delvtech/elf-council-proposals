df19ee1e5e8d810a9308dfcc8d37a764c943cc44
Matthew Brown <mattfdbrown@gmail.com>
2021-10-27 20:22:41 -0700

    build proposals

--- dist/index.js
+++ dist/index.js
@@ -21,23 +21,37 @@ exports.testnetProposals = {
     "snapshotSpace": "element-finance.eth",
     "proposals": [
         {
-            "proposalId": 0,
+            "proposalId": "0",
             "proposalHash": "0x1f33b0550b8cea797c06e3cbe131b0cd810dbe3c33e5d35dbe49e730307aa339",
             "unlock": 11872887,
             "lastCall": 11911862,
-            "created": 1613286486,
+            "created": 11853399,
+            "createdTimestamp": 1613286420,
             "expiration": 11905367,
             "quorum": 1,
+            "targets": [
+                "0x4ed7c70F96B99c776995fB64377f0d4aB3B0e1C1"
+            ],
+            "calldatas": [
+                "0x88b49b834dbdd3e053743c5483a6f5f453200c2c9201e1330e5e5f99997aafcbe4389a2a"
+            ],
             "snapshotId": "QmZSURBMfMh2qSTPSSjjaL2qPdqTMJsfpkvwxuKe72bH3y"
         },
         {
-            "proposalId": 1,
+            "proposalId": "1",
             "proposalHash": "0x1f33b0550b8cea797c06e3cbe131b0cd810dbe3c33e5d35dbe49e730307aa339",
-            "unlock": 11872891,
-            "lastCall": 11911866,
-            "created": 1613286512,
-            "expiration": 11905371,
+            "unlock": 11872896,
+            "lastCall": 11911871,
+            "created": 11853408,
+            "createdTimestamp": 1613392500,
+            "expiration": 11905376,
             "quorum": 1,
+            "targets": [
+                "0x4ed7c70F96B99c776995fB64377f0d4aB3B0e1C1"
+            ],
+            "calldatas": [
+                "0x88b49b834dbdd3e053743c5483a6f5f453200c2c9201e1330e5e5f99997aafcbe4389a2a"
+            ],
             "snapshotId": "0x46b4c3dbdb4b8b84fe42660ac5b5a41b9026c472c22e8a8d4a76ba71bf3dd825"
         }
     ]
