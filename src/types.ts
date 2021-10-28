f5e87d00874c7a7b441a1c9902a0457ab22a2077
Matthew Brown <mattfdbrown@gmail.com>
2021-10-27 20:21:46 -0700

    add stuff to Proposal

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,23 @@
 export declare const testnetProposals: ProposalsJson;
 
 export interface Proposal {
-  proposalHash: string;
-  proposalId: number;
   /**
-   * Timestamp in seconds of the block the proposal was created in.
+   * A hash of the targets and calldatas for the proposal
+   */
+  proposalHash: string;
+  /**
+   * The nonce identifier of the proposal stored in CoreVoting
+   */
+  proposalId: string;
+
+  /**
+   * The block number the proposal was create at
    */
   created: number;
+  /**
+   * Timestamp in seconds of the block the proposal was created in
+   */
+  createdTimestamp: number;
   /**
    * Block number for last vote
    */
@@ -19,7 +30,24 @@ export interface Proposal {
    * Block number for end of execution period
    */
   lastCall: number;
+
+  /**
+   * minimum vote power to pass or reject a proposal
+   */
   quorum: number;
+
+  /**
+   * list of addresses the Timelock contract will call when executing a proposal
+   */
+  targets: string[];
+
+  /**
+   * The calldata associated with each target address, must be the same length as targets
+   */
+  calldatas: string[];
+  /**
+   * id of the snapshot proposal
+   */
   snapshotId: string;
 }
 
