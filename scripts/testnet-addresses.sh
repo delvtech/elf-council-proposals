d705545ca064ad1f23067643f6cee01ea8a17603
efi-devops <72817959+efi-devops@users.noreply.github.com>
2021-10-28 17:42:52 -0700

    Add 3rd proposal to testnet.proposals.json (#2)
    Co-authored-by: Danny Delott <dannydelott@gmail.com>

--- scripts/testnet-addresses.sh
+++ scripts/testnet-addresses.sh
@@ -1,5 +1,5 @@
 # Grab the latest testnet addresses file
-curl https://raw.githubusercontent.com/element-fi/elf-council-testnet/main/src/addresses/addresses.json?token=ARLR2J4IMN4ZKMN53UG3IITBPMYWM > src/testnet.addresses.json
+curl https://raw.githubusercontent.com/element-fi/elf-council-testnet/main/src/addresses/addresses.json?token=ARLR2J5JVHIZOX3VTZQCTFDBQR53I > src/testnet.addresses.json
 
 # Grab thte latest testnet AddresssesJsonFile.ts
-curl https://raw.githubusercontent.com/element-fi/elf-council-testnet/main/src/addresses/AddressesJsonFile.ts?token=ARLR2J3JEPYSIPIW25GY4Y3BPMZEQ> src/AddressesJsonFile.ts
+curl https://raw.githubusercontent.com/element-fi/elf-council-testnet/main/src/addresses/AddressesJsonFile.ts?token=ARLR2J5JVHIZOX3VTZQCTFDBQR53I > src/AddressesJsonFile.ts
