f3a19a42309f92dad753be70de8d19c44827dcd4
Matthew Brown <mattfdbrown@gmail.com>
2022-01-08 00:54:02 -0300

    update dist, add src files

--- dist/index.js
+++ dist/index.js
@@ -15,7 +15,7 @@
  * 🚨🚨🚨 THIS FILE IS AUTO-GENERATED. DO NOT EDIT MANUALLY. 🚨🚨🚨
  */
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.testnetProposals = void 0;
+exports.goerliProposals = exports.testnetProposals = void 0;
 exports.testnetProposals = {
     "version": "0.0.0",
     "snapshotSpace": "element-finance.eth",
@@ -104,6 +104,63 @@ exports.testnetProposals = {
                 "0x88b49b834dbdd3e053743c5483a6f5f453200c2c9201e1330e5e5f99997aafcbe4389a2a"
             ],
             "snapshotId": "0x45bdb2351a21da73162ba018a7b448231945b7754abec0ecdc66c3778e9e7720"
+        }
+    ]
+};
+exports.goerliProposals = {
+    "version": "0.0.0",
+    "snapshotSpace": "element-finance.eth",
+    "proposals": [
+        {
+            "calldatas": [
+                "0x88b49b8372416bb3ac6f3b340cccdbafbf60420e75bb439760acfe30a9078e52e60b6e79"
+            ],
+            "created": 6161440,
+            "createdTimestamp": 1641598308,
+            "expiration": 6161442,
+            "lastCall": 6261439,
+            "proposalHash": "0xd10b00b9ee489761fe2be2637f5cc9a4fb1f8678aaa55f812ce122af3c13c458",
+            "proposalId": "0",
+            "quorum": "100000.0",
+            "snapshotId": "QmZSURBMfMh2qSTPSSjjaL2qPdqTMJsfpkvwxuKe72bH3y",
+            "targets": [
+                "0x36687bdD319a78AB4b4347f3A7459Da235AFc4f4"
+            ],
+            "unlock": 6161441
+        },
+        {
+            "calldatas": [
+                "0x88b49b8372416bb3ac6f3b340cccdbafbf60420e75bb439760acfe30a9078e52e60b6e79"
+            ],
+            "created": 6161448,
+            "createdTimestamp": 1641598428,
+            "expiration": 6161450,
+            "lastCall": 6261446,
+            "proposalHash": "0xd10b00b9ee489761fe2be2637f5cc9a4fb1f8678aaa55f812ce122af3c13c458",
+            "proposalId": "1",
+            "quorum": "100000.0",
+            "snapshotId": "0x46b4c3dbdb4b8b84fe42660ac5b5a41b9026c472c22e8a8d4a76ba71bf3dd825",
+            "targets": [
+                "0x36687bdD319a78AB4b4347f3A7459Da235AFc4f4"
+            ],
+            "unlock": 6161449
+        },
+        {
+            "calldatas": [
+                "0x88b49b8364ec53acdd6f74efcba0de586952c40e23aa87d547d57fabb1ee21203b7b09ea"
+            ],
+            "created": 6161622,
+            "createdTimestamp": 1641601039,
+            "expiration": 6161624,
+            "lastCall": 0,
+            "proposalHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+            "proposalId": "2",
+            "quorum": "100000.0",
+            "snapshotId": "0x0527654d3f94d4798d34ac8ec574da9203f7efe4b4a7a87092fa316abde25932",
+            "targets": [
+                "0x36687bdD319a78AB4b4347f3A7459Da235AFc4f4"
+            ],
+            "unlock": 6161623
         }
     ]
 };
