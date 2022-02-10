84a430a87c3c07150a06eb10799d9b255c3e89fc
Danny Delott <dannydelott@gmail.com>
2022-02-09 16:26:54 -0800

    Remove old proposals

--- src/goerli.ts
+++ src/goerli.ts
@@ -6,7 +6,7 @@ import hre, { ethers } from "hardhat";
 
 import { Proposal, ProposalsJson } from "src/types";
 
-import { SNAPSHOT_SPACE_ID_GOERLI, SNAPSHOT_SPACE_ID_MAINNET } from "src/snapshot";
+import { SNAPSHOT_SPACE_ID_GOERLI } from "src/snapshot";
 import { BigNumber, providers } from "ethers";
 
 const ALCHEMY_GOERLI_RPC_HOST =
@@ -28,41 +28,21 @@ const coreVotingContract = CoreVoting__factory.connect(
  * Note: Sometimes the snapshot proposal ids are IPFS strings or hex strings 🤷
  */
 const snapshotIdsByProposalId: Record<string, string> = {
-  "0": "QmZSURBMfMh2qSTPSSjjaL2qPdqTMJsfpkvwxuKe72bH3y",
-  "1": "0x46b4c3dbdb4b8b84fe42660ac5b5a41b9026c472c22e8a8d4a76ba71bf3dd825",
-  "2": "0x0527654d3f94d4798d34ac8ec574da9203f7efe4b4a7a87092fa316abde25932",
-  "3": "0x7c0bea7eb9340c9bbfcce5ba6b9ca3cbf46e214a7a8f113ab27472378a77aff5",
-  "4": "0x45bdb2351a21da73162ba018a7b448231945b7754abec0ecdc66c3778e9e7720",
-  "5": "0xc9899417b7b6e69ed856d74b6e7e40bcae4a0e2f90cf69547faf79548547d946",
+  "6": "QmZSURBMfMh2qSTPSSjjaL2qPdqTMJsfpkvwxuKe72bH3y",
+  "7": "0x46b4c3dbdb4b8b84fe42660ac5b5a41b9026c472c22e8a8d4a76ba71bf3dd825",
 };
 
 const targetsByProposalId: Record<string, string[]> = {
-  "0": ["0x36687bdD319a78AB4b4347f3A7459Da235AFc4f4"],
-  "1": ["0x36687bdD319a78AB4b4347f3A7459Da235AFc4f4"],
-  "2": ["0x36687bdD319a78AB4b4347f3A7459Da235AFc4f4"],
-  "3": ["0x36687bdD319a78AB4b4347f3A7459Da235AFc4f4"],
-  "4": ["0x36687bdD319a78AB4b4347f3A7459Da235AFc4f4"],
-  "5": ["0x36687bdD319a78AB4b4347f3A7459Da235AFc4f4"],
+  "6": ["0x36687bdD319a78AB4b4347f3A7459Da235AFc4f4"],
+  "7": ["0x36687bdD319a78AB4b4347f3A7459Da235AFc4f4"],
 };
 
 const callDatasByProposalId: Record<string, string[]> = {
-  "0": [
-    "0x88b49b8372416bb3ac6f3b340cccdbafbf60420e75bb439760acfe30a9078e52e60b6e79",
-  ],
-  "1": [
-    "0x88b49b8372416bb3ac6f3b340cccdbafbf60420e75bb439760acfe30a9078e52e60b6e79",
-  ],
-  "2": [
+  "6": [
     "0x88b49b8364ec53acdd6f74efcba0de586952c40e23aa87d547d57fabb1ee21203b7b09ea",
   ],
-  "3": [
+  "7": [
     "0x88b49b8364ec53acdd6f74efcba0de586952c40e23aa87d547d57fabb1ee21203b7b09ea",
-  ],
-  "4": [
-    "0x88b49b836772a5a4807b25bc0b6b5641b858d3363f93421880294675a8f519860f814c74",
-  ],
-  "5": [
-    "0x88b49b836772a5a4807b25bc0b6b5641b858d3363f93421880294675a8f519860f814c74",
   ],
 };
 
