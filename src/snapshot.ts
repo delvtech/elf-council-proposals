e9d8fa46e828e42666b56e7c900eb635129d1c80
Matthew Brown <mattfdbrown@gmail.com>
2022-04-27 09:39:47 -0500

    add gsc propoals json

--- src/snapshot.ts
+++ src/snapshot.ts
@@ -2,7 +2,10 @@ import { gql, request } from "graphql-request";
 
 const SNAPSHOT_GRAPHQL_ENDPOINT = "https://hub.snapshot.org/graphql";
 
-export const SNAPSHOT_SPACE_ID_MAINNET = "element-finance.eth";
+export const SNAPSHOT_SPACE_ID_MAINNET = "elfi.eth";
+
+// TODO: get real name
+export const SNAPSHOT_GSC_SPACE_ID_MAINNET = "elfi-gsc.eth";
 export const SNAPSHOT_SPACE_ID_GOERLI = "element-finance-goerli.eth";
 
 export interface SnapshotProposal {
